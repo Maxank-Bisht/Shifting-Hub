34ea2643adae52406d898fa28d8c66d05e292814
Mayank Bisht <bishtmayank799@gmail.com>
2021-07-30 13:21:50 +0530

    added more functionality

--- src/js/app.js
+++ src/js/app.js
@@ -82,19 +82,142 @@ const UICtrl = (function () {
 
 	return {
 		getState: getState,
-		insertBackground: function (item) {
-			const name = item.name;
-		},
+		// getIconId: async (item) => {
+		// 	const name = item.name;
+		// 	const res = await fetch(
+		// 		`https://search.icons8.com/api/iconsets/v5/search?term=${name}&amount=1&token=mWEovT3GtWeY0h90m8Gu49w4pBxgxUFvnRjFwNBL`
+		// 	);
+		// 	const data = await res.json();
+
+		// 	return data.icons[0].id;
+		// },
+		// searchIcon: async (iconId) => {
+		// 	const res = await fetch(
+		// 		`https://api-icons.icons8.com/publicApi/icons/icon?id=${iconId}&token=mWEovT3GtWeY0h90m8Gu49w4pBxgxUFvnRjFwNBL`
+		// 	);
+		// 	const data = await res.json();
+		// 	return data.icon.svg;
+		// },
 		createItem: function (item) {
 			const div = document.createElement('div');
 			div.id = `${item.id}`;
-			div.className = 'border bg-light border-2 border-dark justify-content-center align-items-center draggable';
-			div.setAttribute(
-				'style',
-				`display:flex;box-sizing:border-box;cursor: grab;height:${item.length}px;width:${item.breadth}px`
-			);
-			div.setAttribute('draggable', 'true');
-			div.innerHTML = item.name;
+			div.className = 'd-flex justify-content-center align-items-center draggable';
+			div.style.boxSizing = 'border-box';
+			div.style.cursor = 'grab';
+			div.style.height = `${item.length}px`;
+			div.style.width = `${item.breadth}px`;
+			div.style.zIndex = '1';
+			// div.setAttribute('style', `display:flex;box-sizing:border-box;cursor: grab;`);
+			// div.setAttribute('draggable', 'true');
+			let img;
+			const name = item.name.toLowerCase();
+			const isSquare = item.length == item.breadth ? true : false;
+			const isHorizontal = item.length > item.breadth ? true : false;
+			if (name.includes('bed')) {
+				if (isSquare) {
+					img = `<img src='src/imgs/bed (2).png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+				} else if (isHorizontal) {
+					img = `<img src='src/imgs/bed (3).png' class='dragIcon' height ='${item.breadth}px' width='${item.length}px' class='rotate90' ></img>`;
+				} else {
+					img = `<img src='src/imgs/bed (3).png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px' ></img>`;
+				}
+			} else if (name.includes('chair')) {
+				img = `<img src='src/imgs/armchair.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+			} else if (name.includes('bath')) {
+				img = `<img src='src/imgs/bath.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+			} else if (name.includes('book')) {
+				img = `<img src='src/imgs/bookshelf.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+			} else if (name.includes('cabinet')) {
+				if (isHorizontal) {
+					img = `<img src='src/imgs/cabinet.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px' ></img>`;
+				} else {
+					img = `<img src='src/imgs/cabinet.png' class='dragIcon' height ='${item.breadth}px' width='${item.length}px' class='rotate90' ></img>`;
+				}
+			} else if (name.includes('car')) {
+				if (isHorizontal) {
+					img = `<img src='src/imgs/car.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px' ></img>`;
+				} else {
+					img = `<img src='src/imgs/car.png' class='dragIcon' height ='${item.breadth}px' width='${item.length}px' class='rotate90' ></img>`;
+				}
+			} else if (name.includes('cupboard') || name.includes('almirah')) {
+				img = `<img src='src/imgs/cupboard.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+			} else if (name.includes('desk')) {
+				img = `<img src='src/imgs/desk.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+			} else if (name.includes('table')) {
+				if (isSquare) {
+					img = `<img src='src/imgs/dining-table.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+				} else if (isHorizontal) {
+					img = `<img src='src/imgs/dining-table (1).png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px' ></img>`;
+				} else {
+					img = `<img src='src/imgs/dining-table (1).png' class='dragIcon' height ='${item.breadth}px' width='${item.length}px' class='rotate90' ></img>`;
+				}
+			} else if (name.includes('door')) {
+				if (isHorizontal) {
+					img = `<img src='src/imgs/door.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px' ></img>`;
+				} else {
+					img = `<img src='src/imgs/door.png' class='dragIcon' height ='${item.breadth}px' width='${item.length}px' class='rotate90' ></img>`;
+				}
+			} else if (name.includes('fridge')) {
+				img = `<img src='src/imgs/fridge.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+			} else if (name.includes('hob')) {
+				img = `<img src='src/imgs/hob.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+			} else if (name.includes('jacuzzi')) {
+				img = `<img src='src/imgs/jacuzzi.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+			} else if (name.includes('plant')) {
+				if (isSquare) {
+					img = `<img src='src/imgs/plant.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+				} else if (isHorizontal) {
+					img = `<img src='src/imgs/plant (1).png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px' ></img>`;
+				} else {
+					img = `<img src='src/imgs/plant (1).png' class='dragIcon' height ='${item.breadth}px' width='${item.length}px' class='rotate90' ></img>`;
+				}
+			} else if (name.includes('sink')) {
+				if (isHorizontal) {
+					img = `<img src='src/imgs/sink.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px' ></img>`;
+				} else {
+					img = `<img src='src/imgs/sink.png' class='dragIcon' height ='${item.breadth}px' width='${item.length}px' class='rotate90' ></img>`;
+				}
+			} else if (name.includes('slipper') || name.includes('shoe')) {
+				if (isHorizontal) {
+					img = `<img src='src/imgs/slippers.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px' ></img>`;
+				} else {
+					img = `<img src='src/imgs/slippers.png' class='dragIcon' height ='${item.breadth}px' width='${item.length}px' class='rotate90' ></img>`;
+				}
+			} else if (name.includes('sofa') || name.includes('couch')) {
+				if (isHorizontal) {
+					img = `<img src='src/imgs/sofa (1).png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px' ></img>`;
+				} else {
+					img = `<img src='src/imgs/sofa (1).png' class='dragIcon' height ='${item.breadth}px' width='${item.length}px' class='rotate90' ></img>`;
+				}
+			} else if (name.includes('toilet')) {
+				if (isHorizontal) {
+					img = `<img src='src/imgs/toilet.png' class='dragIcon' height ='${item.breadth}px' width='${item.length}px' class='rotate90' ></img>`;
+				} else {
+					img = `<img src='src/imgs/toilet.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px' ></img>`;
+				}
+			} else if (name.includes('treadmill')) {
+				if (isHorizontal) {
+					img = `<img src='src/imgs/treadmill.png' class='dragIcon' height ='${item.breadth}px' width='${item.length}px' class='rotate90' ></img>`;
+				} else {
+					img = `<img src='src/imgs/treadmill.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px' ></img>`;
+				}
+			} else if (name.includes('tv') || name.includes('television')) {
+				if (isHorizontal) {
+					img = `<img src='src/imgs/tv.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px' ></img>`;
+				} else {
+					img = `<img src='src/imgs/tv.png' class='dragIcon' height ='${item.breadth}px' width='${item.length}px' class='rotate90' ></img>`;
+				}
+			} else if (name.includes('washbasin')) {
+				img = `<img src='src/imgs/washbasin.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+			} else if (name.includes('workplace')) {
+				img = `<img src='src/imgs/workplace.png' class='dragIcon' height ='${item.length}px' width='${item.breadth}px'></img>`;
+			} else {
+				img = name;
+				div.classList.add('border');
+				div.classList.add('border-3');
+				div.classList.add('border-dark');
+			}
+			div.innerHTML = img;
 			document.querySelector('#playground').insertAdjacentElement('afterbegin', div);
 		},
 		getItemInput: function () {
@@ -117,6 +240,7 @@ const UICtrl = (function () {
 			document.querySelector('#editMessage').style.display = 'none';
 			document.querySelector('#update').style.display = 'none';
 			document.querySelector('#delete').style.display = 'none';
+			document.querySelector('#rotate').style.display = 'none';
 			changeState('normal');
 			UICtrl.clearInputFields();
 		},
@@ -127,6 +251,7 @@ const UICtrl = (function () {
 			document.querySelector('#back').style.display = 'inline-block';
 			document.querySelector('#editMessage').style.display = 'inline-block';
 			document.querySelector('#update').style.display = 'none';
+			document.querySelector('#rotate').style.display = 'none';
 			document.querySelector('#delete').style.display = 'none';
 			changeState('edit');
 		},
@@ -136,6 +261,7 @@ const UICtrl = (function () {
 			document.querySelector('#deleteAll').style.display = 'none';
 			document.querySelector('#back').style.display = 'inline-block';
 			document.querySelector('#editMessage').style.display = 'none';
+			document.querySelector('#rotate').style.display = 'inline-block';
 			document.querySelector('#update').style.display = 'inline-block';
 			document.querySelector('#delete').style.display = 'inline-block';
 		},
@@ -145,11 +271,37 @@ const UICtrl = (function () {
 		clearAllItems: function () {
 			document.querySelector('#playground').innerHTML = '';
 		},
+		rotateItem: function (item) {
+			const id = item.id;
+			const element = document.getElementById(id);
+			let transform = element.style.transform;
+			let newTransform;
+			if (transform.includes('rotate(90deg)')) {
+				newTransform = transform.replace('rotate(90deg)', 'rotate(180deg)');
+			} else if (transform.includes('rotate(180deg)')) {
+				newTransform = transform.replace('rotate(180deg)', 'rotate(270deg)');
+			} else if (transform.includes('rotate(270deg)')) {
+				newTransform = transform.replace('rotate(270deg)', 'rotate(0deg)');
+			} else if (transform.includes('rotate(0deg)')) {
+				newTransform = transform.replace('rotate(0deg)', 'rotate(90deg)');
+			} else {
+				newTransform = transform + ' rotate(90deg)';
+			}
+			element.style.transform = newTransform;
+		},
 	};
 })();
 const AppCtrl = (function (UICtrl, ItemCtrl) {
 	const loadEventListerners = function () {
 		document.querySelector('#add-item').addEventListener('click', addItemSubmit);
+		//increse z-index
+		document.querySelector('#playground').addEventListener('click', (e) => {
+			if (e.target.classList.contains('draggable')) {
+				e.target.style.zIndex = parseInt(e.target.style.zIndex) + 2;
+			} else if (e.target.classList.contains('dragIcon')) {
+				e.target.parentNode.style.zIndex = parseInt(e.target.parentNode.style.zIndex) + 2;
+			}
+		});
 
 		//Edit icon event listenser
 		document.querySelector('#edit').addEventListener('click', editBtnClick);
@@ -163,12 +315,16 @@ const AppCtrl = (function (UICtrl, ItemCtrl) {
 		document.querySelector('#playground').addEventListener('click', itemToEdit);
 		//delete all item
 		document.querySelector('#deleteAll').addEventListener('click', deleteAllItems);
+		//rotate btn
+		document.querySelector('#rotate').addEventListener('click', rotateItemClick);
 	};
 	const addItemSubmit = function (e) {
 		const item = UICtrl.getItemInput();
 		const { name, length, breadth } = item;
 		if (name && length && breadth) {
 			const newItem = ItemCtrl.addItem(name, length, breadth);
+			// const iconId = await UICtrl.getIconId(newItem);
+			// const iconSVG = await UICtrl.searchIcon(iconId);
 			UICtrl.createItem(newItem);
 			UICtrl.clearInputFields();
 		} else {
@@ -180,8 +336,17 @@ const AppCtrl = (function (UICtrl, ItemCtrl) {
 		UICtrl.editIconClick();
 	};
 	const itemToEdit = function (e) {
-		if (UICtrl.getState() === 'edit' && e.target.parentNode.id === 'playground') {
-			const item = ItemCtrl.getItemById(e.target.id);
+		if (
+			UICtrl.getState() === 'edit' &&
+			(e.target.parentNode.parentNode.id === 'playground' || e.target.parentNode.id === 'playground')
+		) {
+			let id;
+			if (e.target.parentNode.parentNode.id === 'playground') {
+				id = e.target.parentNode.id;
+			} else {
+				id = e.target.id;
+			}
+			const item = ItemCtrl.getItemById(id);
 			ItemCtrl.setCurrentItem(item);
 			document.querySelector('#name').value = item.name;
 			document.querySelector('#length').value = item.length;
@@ -211,6 +376,10 @@ const AppCtrl = (function (UICtrl, ItemCtrl) {
 	const deleteAllItems = function () {
 		ItemCtrl.clearAllItems();
 		UICtrl.clearAllItems();
+	};
+	const rotateItemClick = function () {
+		const currentItem = ItemCtrl.getCurrentItem();
+		UICtrl.rotateItem(currentItem);
 	};
 	return {
 		init: function () {
--- src/js/dragEvent.js
+++ src/js/dragEvent.js
@@ -9,26 +9,45 @@ interact('.draggable').draggable({
 		move(event) {
 			position.x += event.dx;
 			position.y += event.dy;
-			event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
+			const transform = event.target.style.transform;
+			if (transform.includes('rotate')) {
+				console.log('Contains rotate');
+				let transforms = transform.split(' ');
+
+				// translate(2.39999px, 0px) rotate(180deg)
+
+				console.log(transforms);
+				if (transforms.length === 1) {
+					event.target.style.transform = `translate(${position.x}px, ${position.y}px) ${transforms[0]}`;
+				} else {
+					transforms[0] = `translate(${position.x}px,`;
+					transforms[1] = `${position.y}px)`;
+					event.target.style.transform = `${transforms[0]} ${transforms[1]} ${transforms[2]}`;
+				}
+			} else {
+				console.log('do not contains rotate');
+				event.target.style.transform = `translate(${position.x}px,${position.y}px)`;
+			}
 		},
 	},
-	// modifiers: [
-	// 	interact.modifiers.restrictRect({
-	// 		restriction: '.box',
-	// 	}),
-	// ],
+	modifiers: [
+		interact.modifiers.restrictRect({
+			restriction: '.box',
+		}),
+	],
 });
 
-interact('.box').dropzone({
-	ondrop: function (event) {
-		drop = true;
-	},
-});
-
-interact('.box').on('dropactivate', function (event) {
-	if (event.target.classList.contains('box')) {
-		console.log('yes');
-	} else {
-		console.log('no');
-	}
-});
+// interact('.dropzone').dropzone({
+// 	checker: function (
+// 		dragEvent, // related dragmove or dragend
+// 		event, // Touch, Pointer or Mouse Event
+// 		dropped, // bool default checker result
+// 		dropzone, // dropzone Interactable
+// 		dropzoneElement, // dropzone element
+// 		draggable, // draggable Interactable
+// 		draggableElement // draggable element
+// 	) {
+// 		console.log(draggable);
+// 		return dropped;
+// 	},
+// });
